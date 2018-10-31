a4cf28f6eae960c66ce12c2d1e153dbab51963a5
roman1k <roman1_k@ukr.net>
2018-10-31 15:22:37 +0200

    change  class Rating

--- src/main/java/progectx/demo/models/Rating.java
+++ src/main/java/progectx/demo/models/Rating.java
@@ -1,20 +1,22 @@
 package progectx.demo.models;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Rating {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    long countOfLikes;
-    double mark;
-
-    double averageOfRating =  Math.round(mark/(double) countOfLikes);
+     private int countOfLikes;
+     private int mark;
+     private  double averageOfRating;
 
 }
