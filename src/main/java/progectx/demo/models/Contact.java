caddef7d503fd3ce8e4ca339a63e258c793e41ca
SydorRoman <romansydor1998@gmail.com>
2018-10-31 14:19:43 +0200

    create gallary

--- src/main/java/progectx/demo/models/Contact.java
+++ src/main/java/progectx/demo/models/Contact.java
@@ -1,10 +1,11 @@
 package progectx.demo.models;
 
+
 public class Contact
 {
     //default
     private String number;
-    private String city;
+    private City city;
     private String mail;
     private String faceBook;
     private String Instagram;
--- src/main/java/progectx/demo/service/impl/Impl/PhotographServiceImp.java
+++ src/main/java/progectx/demo/service/impl/Impl/PhotographServiceImp.java
@@ -2,6 +2,8 @@ package progectx.demo.service.impl.Impl;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import progectx.demo.DAO.PhotographDAO;
 import progectx.demo.models.Photograph;
 import progectx.demo.service.impl.PhotographService;
@@ -11,4 +13,8 @@ public class PhotographServiceImp implements PhotographService {
     @Autowired
     private PhotographDAO photographDAO;
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
