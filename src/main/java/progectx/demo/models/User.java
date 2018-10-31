d0defc3ab6fa3ed0a476625fc6bda23b88981a19
roman1k <roman1_k@ukr.net>
2018-10-31 14:37:20 +0200

    update User

--- src/main/java/progectx/demo/models/User.java
+++ src/main/java/progectx/demo/models/User.java
@@ -1,12 +1,6 @@
 package progectx.demo.models;
 
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.*;
-import java.util.Collection;
-
 
 @Data
 public class User {
