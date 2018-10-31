1d7f75fc4c1450a49431da047ba8a2d2c24cf3af
roman1k <roman1_k@ukr.net>
2018-10-31 15:39:51 +0200

    Create Admin + CustamerDAO

--- src/main/java/progectx/demo/models/Admin.java
+++ src/main/java/progectx/demo/models/Admin.java
@@ -3,9 +3,15 @@ package progectx.demo.models;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Data
-public class Admin {
+public class Admin  extends  User{
+    @Id
+    @GeneratedValue()
+    private  int Id;
+
 
 }
